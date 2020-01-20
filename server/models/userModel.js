c554a3586b4e0a6fcba152407547eab05d1277d2
Arlo Guiwo <arloguiwo@gmail.com>
2020-01-20 18:54:09 -0500

    Added user model file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,8 +1,15 @@
-const { Client } = require('pg');
-const client = new Client();
+const { Pool , Client } = require('pg');
 const connectionString = 'postgres://ghizlawh:9sejhfX5XfjqVnGp5DiB3dB-6x1oYZC-@rajje.db.elephantsql.com:5432/ghizlawh';
 
+const pool = new Pool({
+    connectionString,
+});
 
-client.connect();
+module.exports = {
+    query: (text, params, callback) => {
+      return pool.query(text, params, callback)
+    },
+  }
+
 
 
