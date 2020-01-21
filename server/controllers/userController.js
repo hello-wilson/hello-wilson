af93fb0d7d4e3f33924beabe6a38b06652a5eac4
Arlo Guiwo <arloguiwo@gmail.com>
2020-01-21 11:49:12 -0500

    created login route handler in server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,19 @@
 import { query } from '../models/dataModel';
+
+const userController = {
+
+
+//queries the database using username and password found in the body
+login (){
+
+}
+
+//queries the database using username and password found in the body to create a new user. First validates data on the request object and returns success or failure depending on if conditionals pass and query is successful.
+
+signup (){
+
+}
+
+}
+
+export userController;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const path = require('path')
 const express = require('express');
 const app = express();
+const userController = require('./controllers/userController');
+const messageController = require('./controllers/messageController');
+const bcrypt = require('bcrypt');
 
 
 app.get('/', (req, res) => {
     res.send('hello')
 })
 
-app.get('/login', (req, res) => {
+
+
+app.post('/signup', (req, res) => {
     res.json(res.locals.loginStatus);
 })
+
+
 
 app.listen(3000,() => {
     console.log('Server listening on PORT 3000')
