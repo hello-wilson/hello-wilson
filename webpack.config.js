f4d26b95ab52cbd87dad3ac6889815f3199059c4
Joseph Michael Corrado <joeyycorrss@gmail.com>
2020-01-21 12:23:34 -0500

    updated webpack and package.json for React use

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
